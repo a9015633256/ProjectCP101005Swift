075b13588aad3814eb70368c591cc03596d713d1
yangwensing <a9015633256@hotmail.com>
2018-08-13 18:02:48 +0800

    20180813更新，修改個人檔案，更新圖片抓起，更新部分bug

--- ProjectCP101005Swift/WenSing/TeacherReplaceFileViewController.swift
+++ ProjectCP101005Swift/WenSing/TeacherReplaceFileViewController.swift
@@ -12,15 +12,25 @@ import MobileCoreServices
 
 class TeacherReplaceFileViewController: UIViewController{
    
-    let communicator = CommunicatorWenSing()
+    var communicator = CommunicatorWenSing()
+    
+    var communicator2: CommunicatorMingTa?
     
     var teacherFile = [TeachersFile2]()
     
+    var teacherUploadFile = TeachersFile()
+    
     var jpgData: Data?
     
-    var datePicker : UIDatePicker!
+    let datepicker = UIDatePicker()
+    
+    var date = "1990-01-01"
+    
+    let dateformater = DateFormatter()
     
     var pickerLabel : UILabel!
+    
+    var teacherImageDictionary = UIImage()
     
     @IBOutlet weak var teacherImageView: UIImageView!
     @IBOutlet weak var accountInputText: UITextField!
@@ -37,6 +47,7 @@ class TeacherReplaceFileViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createDatePicker()
         
         //假資料
         let teacherId = UserDefaults.standard.string(forKey: "account")
@@ -48,12 +59,6 @@ class TeacherReplaceFileViewController: UIViewController{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    //選擇生日
-    @IBAction func birthdayPicker(_ sender: UITextField) {
-        dataPicker()
-    }
-    
     
     //拍照按鈕
     @IBAction func takePhotoShotBtn(_ sender: Any) {
@@ -80,6 +85,95 @@ class TeacherReplaceFileViewController: UIViewController{
     
     //更改檔案內容
     func conformToReplaceFile() {
+        //getTeacher_Account getTeacher_Email getTeacher_Gender getTeacher_Phone getTeacher_TakeOfficeDate   getId
+        //ACTION_UPDATE_PROFILE上傳的action
+        
+        //抓取輸入資料
+        teacherUploadFile.Teacher_Account = accountInputText.text
+        teacherUploadFile.Teacher_Email = nameInputText.text
+        
+        if genderInputText.selectedSegmentIndex == 0 {
+            teacherUploadFile.Teacher_Gender = 1
+        }else {
+            teacherUploadFile.Teacher_Gender = 2
+        }
+        
+        teacherUploadFile.Teacher_Phone = phoneNumberInputText.text
+        
+        teacherUploadFile.id = UserDefaults.standard.integer(forKey: "uploadThisOne")
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        teacherUploadFile.Teacher_TakeOfficeDate = birthInputText.text
+        
+        let jsonEncoder = JSONEncoder()
+        
+        guard let teacher = try? jsonEncoder.encode(teacherUploadFile) else {
+            assertionFailure()
+            return
+        }
+        
+        guard let teacherString = String(data: teacher, encoding: .utf8) else {
+            assertionFailure()
+            return
+        }
+        
+        var dic: [String: Any] = ["action" : ACTION_UPDATE_PROFILE, "teacherList" : teacherString]
+        
+        if let jpgData = jpgData {
+            let photoBase64 = jpgData.base64EncodedString()
+            dic["Teacher_Photo"] = photoBase64
+        }
+        
+        guard let data = try? JSONSerialization.data(withJSONObject: dic, options: []) else {
+            assertionFailure()
+            return
+        }
+        
+        guard let url = URL(string:TEACHER_LIST_SERVLET ) else {
+            assertionFailure()
+            return
+        }
+        
+        
+        communicator2 = CommunicatorMingTa(targetURL: url)
+        
+        communicator2?.download(from: data, doneHandler: { (error, data) in
+            
+            if let error = error {
+                print("error update student info: \(error)")
+                let alert = UIAlertController(title: "更新失敗", message: nil, preferredStyle: .alert)
+                let ok = UIAlertAction(title: "確定", style: .default, handler: nil)
+                alert.addAction(ok)
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            guard let data = data else {
+                assertionFailure()
+                return
+            }
+            
+            guard let count = String(data: data, encoding: .utf8), count == "1" else {
+                
+                let alert = UIAlertController(title: "更新失敗", message: nil, preferredStyle: .alert)
+                let ok = UIAlertAction(title: "確定", style: .default, handler: nil)
+                alert.addAction(ok)
+                self.present(alert, animated: true, completion: nil)
+                return
+                
+            }
+            
+            let alert = UIAlertController(title: "更新成功", message: nil, preferredStyle: .alert)
+            let ok = UIAlertAction(title: "確定", style: .default, handler: { (action) in
+                self.navigationController?.popViewController(animated: true)
+            })
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+            
+        })
+        
         
     }
     
@@ -122,15 +216,90 @@ class TeacherReplaceFileViewController: UIViewController{
             default:
                 self.genderInputText.selectedSegmentIndex = 0
             }
+            //取得圖片
+            let teacherId2 = self.teacherFile[0].id
+            UserDefaults.standard.set(teacherId2, forKey: "uploadThisOne")
+            self.getFriendImage(teacherId: teacherId2!)
             
         }
         
     }
     
+    func getFriendImage(teacherId: Int){
+        
+        let action = GetImageAction(action: ACTION_GET_IMAGE, id: teacherId, imageSize: 150)
+        // 準備將資料轉為JSON
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .init()
+        // 轉為JSON
+        guard let uploadData = try? encoder.encode(action) else {
+            assertionFailure("JSON encode Fail")
+            return
+        }
+        // 送資料 and 解析回傳的JSON資料
+        communicator.doPost(url: TEACHER_LIST_SERVLET, data: uploadData) { (result) in
+            
+            guard let result = result else {
+                assertionFailure("get data fail")
+                return
+            }
+            
+            guard let image = UIImage.init(data: result) else {
+                return
+            }
+            
+            self.teacherImageDictionary = image
+            print("搜尋圖片\(result)")
+            self.teacherImageView.image = image
+        }
+        
+    }
     
-    
+    //返回鍵
     @IBAction func backBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    //模組化時鐘，只需要修改IBoutlet
+    func createDatePicker(){
+        dateformater.dateFormat = "YYYY-MM-dd"
+        let newdate = dateformater.date(from: date)
+        datepicker.datePickerMode = .date
+        datepicker.setDate(newdate!, animated: true)
+        datepicker.locale = Locale(identifier: "zh_TW")
+        
+        birthInputText.inputView = datepicker
+        datepicktoolbar()
+    }
+    
+    func datepicktoolbar() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "確定", style: .plain, target: self, action: #selector(happybirthdayclicker))
+        toolbar.setItems([doneButton], animated: false)
+        birthInputText.inputAccessoryView = toolbar
+        
+    }
+    
+    @objc//人數#selector
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    @objc
+    func happybirthdayclicker() {
+        dateformater.dateFormat = "YYYY-MM-dd"
+        
+        birthInputText.text = dateformater.string(from: datepicker.date)
+        self.view.endEditing(true)
+    }
+    
+}
+
+extension TeacherReplaceFileViewController: UITextFieldDelegate {
+    
+    //stop dayOfBirth changed by input manually
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return false
     }
     
 }
@@ -180,11 +349,10 @@ extension TeacherReplaceFileViewController: UIImagePickerControllerDelegate, UIN
 //    var datePicker : UIDatePicker!
 //    var pickerLabel : UILabel!
     func dataPicker() {
-        datePicker.datePickerMode = .date
-        datePicker.date = NSDate() as Date
-        datePicker.addTarget(self, action: #selector(show(datePicker:)), for: .valueChanged)
-        birthInputText.inputView = datePicker
-        
+        datepicker.datePickerMode = .date
+        datepicker.date = NSDate() as Date
+        datepicker.addTarget(self, action: #selector(show(datePicker:)), for: .valueChanged)
+        birthInputText.inputView = datepicker
         
     }
     
